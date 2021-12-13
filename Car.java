44c401d32f4a9efe8bfdb62ec9c41215622891fb
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-13 20:30:31 +0000

    edit

--- Car.java
+++ Car.java
@@ -3,6 +3,8 @@ import java.util.InputMismatchException;
 
 final class Car {
 
+  public int times = 11;
+
   /**
   * Prevent instantiation
   * Throw an exception IllegalStateException.
@@ -24,21 +26,28 @@ final class Car {
     final Scanner userInput = new Scanner(System.in);
     System.out.println("Input the color of the vehicle: ");
     final String carColor = userInput.nextLine();
-    System.out.println("Input the plate number: ");
-    final String carPlate = userInput.nextLine();
-    Vehicle car1 = new Vehicle(carColor, carPlate, 2);
-    Vehicle car2 = new Vehicle();
-    System.out.println("Speed of both cars is 0.");
-    System.out.println(car1.Accelerate());
-    for (int accel = 0; accel < 11; accel++) {
-      System.out.println(car2.Accelerate());
+    try {
+      final int checker = Integer.valueOf(carColor);
+    } catch (InputMismatchException errorCode) {
+      System.out.println("Input the plate number: ");
+      final String carPlate = userInput.nextLine();
+      Vehicle car1 = new Vehicle(carColor, carPlate, 2);
+      Vehicle car2 = new Vehicle();
+      System.out.println("Speed of both cars is 0.");
+      System.out.println(car1.Accelerate());
+      for (int accel = 0; accel < times; accel++) {
+        System.out.println(car2.Accelerate());
+      }
+      System.out.println(car1.Brake());
+      System.out.println(car2.Brake());
+      String value1 = car1.Value();
+      String value2 = car2.Value();
+      System.out.println(value1);
+      System.out.println(value2);
+      System.out.println("\nDone.");
+      System.exit(0);
     }
-    System.out.println(car1.Brake());
-    System.out.println(car2.Brake());
-    String value1 = car1.Value();
-    String value2 = car2.Value();
-    System.out.println(value1);
-    System.out.println(value2);
-    System.out.println("\nDone.");
+    System.out.println("Car color must be a string.");
+    System.exit(0);
   }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -1,44 +1,79 @@
 public class Vehicle {
 
-  private String plateNumber = "0";
+  /**
+  * The placeholder for the plate number.
+  */
+  private String plateNumber;
+  /**
+  * The placeholder for the car's color.
+  */
+  private String carColor;
 
-  private String carColor = "";
-
+  /**
+  * The placeholder for the car number.
+  */
   private int carNum = 0;
 
-  private static int doorAmount = 4;
+  /**
+  * The amount of doors the car has.
+  */
+  private int doorAmount = 4;
 
+  /**
+  * The starting speed of the car.
+  */
   private int speed = 0;
 
-  private static int maxSpeed = 100;
+  /**
+  * The max speed of the car.
+  */
+  private int maxSpeed = 100;
 
+  /**
+  *
+  */
   public Vehicle() {
     carColor = "red";
     plateNumber = "12E4S6";
     carNum = 1;
   }
 
-  public Vehicle(String color, String plate, int num) {
+  /**
+  * The Vehicle constructor.
+  */
+  public Vehicle(final String color, final String plate, final int num) {
     plateNumber = plate;
     carColor = color;
     carNum = num;
   }
 
+  /**
+  * The Accelerate method.
+  */
   public String Accelerate() {
     speed += 10;
     if (speed > maxSpeed) {
       return "Car speed cannot be higher than 100km/h";
     } else {
-      return "Car speed for car " + String.valueOf(carNum) + " is now " + String.valueOf(speed);
+      return "Car speed for car " + String.valueOf(carNum)
+      + " is now " + String.valueOf(speed);
     }
   }
 
+  /**
+  * The Brake method.
+  */
   public String Brake() {
     speed = 0;
-    return "Speed of car " + String.valueOf(carNum) + " is " + String.valueOf(speed) + " after braking.";
+    return "Speed of car " + String.valueOf(carNum)
+    + " is " + String.valueOf(speed) + " after braking.";
   }
 
+  /**
+  * The value method.
+  */
   public String Value() {
-    return "For car " + String.valueOf(carNum) + ", car color is " + carColor + " and the plate number is " + plateNumber;
+    return "For car " + String.valueOf(carNum) + ", car color is "
+    + carColor + " and the plate number is " + plateNumber;
   }
 }
